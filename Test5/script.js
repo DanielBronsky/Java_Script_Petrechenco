775d645ea4584d87fa28556ee33489f4ff55b345
Daniel <bronskydaniel4@gmail.com>
2023-08-11 14:51:16 +0300

    Test 5

--- Test5/script.js
+++ Test5/script.js
@@ -1,17 +1,40 @@
-const array = [1,2,3,4,5,6,7,8,9];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const arrayEvenNumbers = [];
 const oddEvenNumbers = [];
 
 function evenNumbers() {
-    for (let i = 1; i < array.length; i++) {
-        if (array[i]%2 ===0) {
-           arrayEvenNumbers.push(array[i])
-        }else {
-            oddEvenNumbers.push(array[i])
-        }
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      arrayEvenNumbers.push(array[i]);
+    } else {
+      oddEvenNumbers.push(array[i]);
     }
+  }
 }
 
-evenNumbers()
+evenNumbers();
 console.log(arrayEvenNumbers);
 console.log(oddEvenNumbers);
+
+let result = "";
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+  for (let j = 0; j < i; j++) {
+    result += "*";
+  }
+  result += "\n";
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First level: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second level: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first;
+      console.log(`Third level: ${k}`);
+    }
+  }
+}
