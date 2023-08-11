bffa9be687b70ee8a4fbb4f6489801b01d7c1a6b
Daniel <bronskydaniel4@gmail.com>
2023-08-11 11:08:36 +0300

    Test 4

---
+++ Test4/script.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt("Сколько фильмов вы уше посмотрели ?" ,"")
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+    
+}
+
+for (let i = 0; i < numberOfFilms; i++) {
+    const lastMovie = prompt("Один из последних просмотренных фильмов?", "");
+    const ratingMovie = prompt("На сколько оцените его?", "");
+
+    personalMovieDB.movies[lastMovie] = ratingMovie;
+}
+
+console.log(personalMovieDB)
